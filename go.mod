60f88b93ae0175dcf968c60d253622489e38ba27
YoshiBot <surke@outlook.es>
2024-03-27 13:51:31 -0500

    pruebas audio

--- go.mod
+++ go.mod
@@ -2,11 +2,14 @@ module chalonland-bot-go
 
 go 1.21.6
 
-require github.com/bwmarrin/discordgo v0.27.1
+require (
+	github.com/bwmarrin/discordgo v0.27.1
+	github.com/joho/godotenv v1.5.1
+	gopkg.in/hraban/opus.v2 v2.0.0-20230925203106-0188a62cb302
+)
 
 require (
 	github.com/gorilla/websocket v1.4.2 // indirect
-	github.com/joho/godotenv v1.5.1 // indirect
 	golang.org/x/crypto v0.0.0-20210421170649-83a5a9bb288b // indirect
 	golang.org/x/sys v0.0.0-20201119102817-f84b799fce68 // indirect
 )
--- main.go
+++ main.go
@@ -2,15 +2,22 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"log"
 	"os"
+	"os/signal"
 	"strings"
+	"time"
 
 	"github.com/bwmarrin/discordgo"
+	"github.com/joho/godotenv"
+	"gopkg.in/hraban/opus.v2"
 )
 
 func main() {
+	godotenv.Load()
 	token := os.Getenv("BOT_TOKEN")
+	println(token)
 	prefix := "."
 	sess, err := discordgo.New("Bot " + token)
 	if err != nil {
@@ -27,6 +34,39 @@ func main() {
 			return
 		}
 	})
+	sess.AddHandler(func(s *discordgo.Session, m *discordgo.VoiceStateUpdate) {
+		if m.Member.User.Bot {
+			return
+		}
+		voiceConnect, _ := sess.ChannelVoiceJoin(m.GuildID, m.ChannelID, false, true)
+
+		f, err := os.Open("speech_32kbps_fb.wav")
+		if err != nil {
+
+		}
+		opusParse, err := opus.NewStream(f)
+		if err != nil {
+
+		}
+		defer s.Close()
+		buf := make([]byte, 16384)
+		for {
+			n, err := opusParse.Read(buf)
+			if err == io.EOF {
+				break
+			} else if err != nil {
+
+			}
+			pcm := buf[:n*2]
+			voiceConnect.OpusSend <- pcm
+
+			// send pcm to audio device here, or write to a .wav file
+
+		}
+		time.Sleep(2 * time.Second)
+		voiceConnect.Disconnect()
+
+	})
 
 	sess.Identify.Intents = discordgo.IntentsAll
 
@@ -34,6 +74,15 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer sess.Close()
 	fmt.Println("El bot esta online")
+
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, os.Interrupt)
+	log.Println("Press Ctrl+C to exit")
+	<-stop
+
+	err = sess.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
 }
